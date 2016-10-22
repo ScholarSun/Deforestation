43bca72e3ec24b92d7c48159712dc4f004676e97
StarryNightSun <sasisc914@gmail.com>
2016-10-22 14:58:14 -0400

    Downloads Sat images, Channels, and Parses JSon

--- src/Data_Processing.java
+++ src/Data_Processing.java
@@ -6,7 +6,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.util.ArrayList;
+import java.util.*;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -18,34 +18,99 @@ public class Data_Processing {
 
 		System.out.println("Testing 1 - Send Http GET request");
 		String resp = http.sendGet();
-		String [] blo = extract(resp);
-		
+		ArrayList<String> hey = extract(resp);
+		downloadSat(hey);
+
 	}
-	
-	public void downloadSat(String saturl) throws Exception{
-		URL website = new URL("https://s3-us-west-2.amazonaws.com/landsat-pds/L8/233/087/LC82330872016239LGN00/LC82330872016239LGN00_B11.TIF");
-		ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-		FileOutputStream fos = new FileOutputStream("information.TIF");
-		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+	public static void downloadSat(ArrayList<String> bob) throws Exception {
+		int red = 0;
+		int blue = 0;
+		int green = 0;
+		for (int x = 0; x < bob.size(); x++) {
+			if (bob.get(x).contains("Red")) {
+				URL website = new URL(bob.get(x - 1));
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				FileOutputStream fos = new FileOutputStream("pics/Red" + red + ".TIF");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				red = red + 1;
+			}
+			if (bob.get(x).contains("Blue")) {
+				URL website = new URL(bob.get(x - 1));
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				FileOutputStream fos = new FileOutputStream("pics/Blue" + blue + ".TIF");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				blue = blue + 1;
+			}
+			if (bob.get(x).contains("Green")) {
+				URL website = new URL(bob.get(x - 1));
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				FileOutputStream fos = new FileOutputStream("pics/Green" + green + ".TIF");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				green = green + 1;
+			}
+		}
 	}
-	
-	public static String[] extract(String responsesat){
-		ArrayList<Integer> indexes = new ArrayList();
-		while(1<3){
-		int miniind = responsesat.indexOf("band");
-		if(miniind == -1){
-			break;
+
+	public static ArrayList<String> extract(String responsesat) {
+		ArrayList<String> bandsndown = new ArrayList();
+		// ArrayList<String> download = new ArrayList();
+		responsesat.indexOf("bands");
+		responsesat.indexOf("download_path");
+		List<String> items = Arrays.asList(responsesat.split(","));
+
+		for (int i = 0; i < items.size(); i++) {
+			if (items.get(i).contains("band")) {
+				String specband = items.get(i).substring(8, items.get(i).length());
+				bandsndown.add(specband);
+			}
+			if (items.get(i).contains("download_path")) {
+				String lol = items.get(i).substring(17, items.get(i).length());
+				bandsndown.add(lol);
+			}
+		}
+
+		for (int i = 0; i < bandsndown.size(); i++) {
+			if (!(bandsndown.get(i).contains("https://"))) {
+				if (!((bandsndown.get(i).contains("Blue") || bandsndown.get(i).contains("Green")
+						|| bandsndown.get(i).contains("Red")))) {
+					// System.out.println(bandsndown.get(i));
+					bandsndown.remove(i);
+					bandsndown.remove(i - 1);
+					i = i - 2;
+				}
+			}
+		}
+
+		for (int x = 0; x<bandsndown.size();x++) {
+			//System.out.println(bandsndown.get(x));
+			if(bandsndown.get(x).contains("https")){
+				String alter = bandsndown.get(x);
+				alter = alter.trim();
+				alter = alter.substring(1, alter.length()-1);
+				bandsndown.set(x, alter);
+			}
 		}
 		
-		responsesat = responsesat.substring(miniind,responsesat.length());
-		
+		ArrayList<String>dupe = new ArrayList<String>();
+		int y=0;
+		for(int x =0;x<bandsndown.size();x++){
+			if(!(dupe.contains(bandsndown.get(x)))){
+			dupe.add(y,bandsndown.get(x));
+			y++;
+			}
 		}
+		bandsndown=dupe;
+		for(String d:bandsndown){
+			//System.out.println(d);
+		}
+		return bandsndown;
 	}
-	
+
 	// HTTP GET request
 	private String sendGet() throws Exception {
 
-		String url = "https://api.skywatch.co/data/time/2016-08-25T13:00:00.0000+0000/location/-71.1043443253471,-42.3150676015829,71.1043443253471,42.3150676015829/source/LANDSAT-8/3000/blue";
+		String url = "https://api.skywatch.co/data/time/2016-08-25T13:00:00.0000+0000/location/-7.16245,-61.5836,28.83755,-33.5836/source/LANDSAT-8/3000/";
 
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -53,15 +118,14 @@ public class Data_Processing {
 		// optional default is GET
 		con.setRequestMethod("GET");
 
-		//add request header
+		// add request header
 		con.setRequestProperty("x-api-key", "htp4p898jb3VJrqrgv0DY1PDbCOPpv66G8gNjUJg");
-		
+
 		int responseCode = con.getResponseCode();
 		System.out.println("\nSending 'GET' request to URL : " + url);
 		System.out.println("Response Code : " + responseCode);
 
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		String inputLine;
 		StringBuffer response = new StringBuffer();
 
@@ -70,9 +134,9 @@ public class Data_Processing {
 		}
 		in.close();
 
-		//print result
-		System.out.println(response.toString());
-		
+		// print result
+		// System.out.println(response.toString());
+
 		String answer = response.toString();
 		return answer;
 	}
